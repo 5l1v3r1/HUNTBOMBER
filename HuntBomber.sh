fe5e92aa635ea911d7c78467988322f3104a47d8
Hunter11821 <68858839+Hunter11821@users.noreply.github.com>
2020-08-09 15:28:30 +0530

    Add files via upload

---
+++ HuntBomber.sh
@@ -0,0 +1,177 @@
+#!/bin/bash
+clear
+echo -e "\033[1;30m           ▄▄    ▄▄  ▄▄    ▄▄  ▄▄▄   ▄▄  ▄▄▄▄▄▄▄▄             "
+echo -e "\033[1;31m           ██    ██  ██    ██  ███   ██  ▀▀▀██▀▀▀             "
+echo -e "\033[1;32m           ██    ██  ██    ██  ██▀█  ██     ██                "
+echo -e "\033[1;33m           ████████  ██    ██  ██ ██ ██     ██                "
+echo -e "\033[1;34m           ██    ██  ██    ██  ██  █▄██     ██                "
+echo -e "\033[1;35m           ██    ██  ▀██▄▄██▀  ██   ███     ██                "
+echo -e "\033[1;36m           ▀▀    ▀▀    ▀▀▀▀    ▀▀   ▀▀▀     ▀▀                "
+echo -e "\033[1;30m  ▄▄▄▄▄▄      ▄▄▄▄   ▄▄▄  ▄▄▄  ▄▄▄▄▄▄    ▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄     "
+echo -e "\033[1;31m  ██▀▀▀▀██   ██▀▀██  ███  ███  ██▀▀▀▀██  ██▀▀▀▀▀▀  ██▀▀▀██    "
+echo -e "\033[1;32m  ██     ██  ██  ██  ████████  ██    ██  ██        ██    ██   "
+echo -e "\033[1;33m  ████████   ██  ██  ██ ██ ██  ███████   ███████   ███████    "
+echo -e "\033[1;34m  ██     ██  ██  ██  ██ ▀▀ ██  ██    ██  ██        ██   ▀██▄  "
+echo -e "\033[1;35m  ██▄▄▄▄██   ██▄▄██  ██    ██  ██▄▄▄▄██  ██▄▄▄▄▄▄  ██     ██  "
+echo -e "\033[1;36m  ▀▀▀▀▀▀▀     ▀▀▀▀   ▀▀    ▀▀  ▀▀▀▀▀▀▀   ▀▀▀▀▀▀▀▀  ▀▀     ▀▀  "
+echo -e "\e[4;45m This SMS AND CALL BOMBER MADE BY Hunter11821 \e[0m"
+echo -e "\e[1;42m HUNTBOMBER \e[0m"
+echo -e "\e[1;44m Press Enter To Continue \e[0m"
+read a1
+if [[ -s hunter.11821 ]];then
+echo -e "\e[1;34m All Requirements Found... \e[0m"
+else
+echo -e '\e[1;36m Installing Requirements... \e[0m'
+echo .
+echo .
+apt install figlet toilet python curl -y
+apt install python3-pip
+pip install -r requirements.txt
+echo This Script Was Made By Hunter11821 >hunter.11821
+echo Requirements Installed....
+echo Press Enter To Continue...
+read upd
+fi
+while :
+do
+rm *.xxx >/dev/null 2>&1
+clear
+echo -e "\e[1;31m"
+
+echo -e "\033[1;36m       ┌────────────────────────────────────────────┐"
+echo -e "\033[1;30m       │░░░░░░░░░█▀▀░█▀▄░█▀▀░█▀█░▀█▀░█▀▀░█▀▄░░░░░░░░│"
+echo -e "\033[1;31m       │░░░░░░░░░█░░░█▀▄░█▀▀░█▀█░░█░░█▀▀░█░█░░░░░░░░│"
+echo -e "\033[1;32m       │░░░░░░░░░▀▀▀░▀░▀░▀▀▀░▀░▀░░▀░░▀▀▀░▀▀░░░░░░░░░│"
+echo -e "\033[1;33m       │░░░░░░░░░░░░░░░░░░░█▀▄░█░█░░░░░░░░░░░░░░░░░░│"
+echo -e "\033[1;34m       │░░░░░░░░░░░░░░░░░░░█▀▄░░█░░░░░░░░░░░░░░░░░░░│"
+echo -e "\033[1;35m       │░░░░░░░░░░░░░░░░░░░▀▀░░░▀░░░░░░░░░░░░░░░░░░░│"
+echo -e "\033[1;36m       │░█░█░█░█░█▀█░▀█▀░█▀▀░█▀▄░▀█░░▀█░░▄▀▄░▀▀▄░▀█░│"
+echo -e "\033[1;30m       │░█▀█░█░█░█░█░░█░░█▀▀░█▀▄░░█░░░█░░▄▀▄░▄▀░░░█░│"
+echo -e "\033[1;35m       │░▀░▀░▀▀▀░▀░▀░░▀░░▀▀▀░▀░▀░▀▀▀░▀▀▀░░▀░░▀▀▀░▀▀▀│"
+echo -e "\033[1;36m       └────────────────────────────────────────────┘"
+echo -e "\033[1;30m           ▄▄    ▄▄  ▄▄    ▄▄  ▄▄▄   ▄▄  ▄▄▄▄▄▄▄▄             "
+echo -e "\033[1;31m           ██    ██  ██    ██  ███   ██  ▀▀▀██▀▀▀             "
+echo -e "\033[1;32m           ██    ██  ██    ██  ██▀█  ██     ██                "
+echo -e "\033[1;33m           ████████  ██    ██  ██ ██ ██     ██                "
+echo -e "\033[1;34m           ██    ██  ██    ██  ██  █▄██     ██                "
+echo -e "\033[1;35m           ██    ██  ▀██▄▄██▀  ██   ███     ██                "
+echo -e "\033[1;36m           ▀▀    ▀▀    ▀▀▀▀    ▀▀   ▀▀▀     ▀▀                "
+echo -e "\033[1;30m  ▄▄▄▄▄▄      ▄▄▄▄   ▄▄▄  ▄▄▄  ▄▄▄▄▄▄    ▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄     "
+echo -e "\033[1;31m  ██▀▀▀▀██   ██▀▀██  ███  ███  ██▀▀▀▀██  ██▀▀▀▀▀▀  ██▀▀▀██    "
+echo -e "\033[1;32m  ██     ██  ██  ██  ████████  ██    ██  ██        ██    ██   "
+echo -e "\033[1;33m  ████████   ██  ██  ██ ██ ██  ███████   ███████   ███████    "
+echo -e "\033[1;34m  ██     ██  ██  ██  ██ ▀▀ ██  ██    ██  ██        ██   ▀██▄  "
+echo -e "\033[1;35m  ██▄▄▄▄██   ██▄▄██  ██    ██  ██▄▄▄▄██  ██▄▄▄▄▄▄  ██     ██  "
+echo -e "\033[1;36m  ▀▀▀▀▀▀▀     ▀▀▀▀   ▀▀    ▀▀  ▀▀▀▀▀▀▀   ▀▀▀▀▀▀▀▀  ▀▀     ▀▀  "
+echo -e "\033[1;30m      ┌──────────────────────────────────────────────┐"
+echo -e "\033[1;31m      │                   SUBSCRIBE                  │"
+echo -e "\033[1;32m      │                OFFICIAL HUNTER               │"
+echo -e "\033[1;33m      └──────────────────────────────────────────────┘"
+echo -e "\033[1;34m      ┌──────────────────────────────────────────────┐"
+echo -e "\033[1;35m      │              Ｍｕｓｔ  Ｊｏｉｎ              │"
+echo -e "\033[1;36m      │        Ｔｅｌｅｇｒａｍ  Ｃｈａｎｎｅｌ      │"
+echo -e "\033[1;30m      │        Ｆｏｒ  Ｌａｔｅｓｔ  Ｌｏｏｔｓ      │"
+echo -e "\033[1;31m      │                                              │"
+echo -e "\033[1;32m      │        ｔ．ｍｅ／ｈｕｎｔｅｒ１１８２１      │"
+echo -e "\033[1;33m      └──────────────────────────────────────────────┘"
+echo -e "\033[1;34m      ┌──────────────────────────────────────────────┐"
+echo -e "\033[1;35m      │        Creator :- @Official Hunter           │"
+echo -e "\033[1;36m      │        Published By :- @Hunter11821          │"
+echo -e "\033[1;30m      │        Support :- Technical support          │"
+echo -e "\033[1;31m      └──────────────────────────────────────────────┘"
+echo -e "\e[4;34m This Bomber Was Created By Hunter11821 \e[0m"
+echo -e "\e[1;34m For Any Problem Contact Me on telegram \e[0m"
+echo -e "\e[1;41m    Telegram ID :- Hunter11821 \e[0m"
+echo -e "\e[1;41m    Telegram Channle :- telegram.dog/c/Hunter11821 \e[0m"
+echo -e "\e[1;41m    YouTube Channle: https://www.youtube.com/c/UCndBK_Y6QHxLXZ2XLrf4zoQ \e[0m"
+echo " "
+echo -e "\e[4;31m Please Read Instruction Carefully !!! \e[0m"
+echo " "
+echo -e "\e[1;42m Press 1 : Start SMS Bomber \e[0m"
+echo -e "\e[1;42m Press 2 : Start Call Bomber \e[0m"
+echo -e "\e[1;42m Press 3 : Update (Works On Linux And Linux Emulators) \e[0m"
+echo -e "\e[1;42m Press 4 : View Features \e[0m"
+echo -e "\e[1;42m Press 5 : Exit \e[0m"
+read ch
+if [ $ch -eq 1 ];then
+clear
+echo -e "\e[1;32m"
+rm *.xxx >/dev/null 2>&1
+python3 hunter.py
+rm *.xxx >/dev/null 2>&1
+exit 0
+elif [ $ch -eq 2 ];then
+clear
+echo -e "\e[1;32m"
+echo 'Call Bomb By Hunter11821'> call.xxx
+python3 hunter.py call
+rm *.xxx >/dev/null 2>&1
+exit 0
+elif [ $ch -eq 3 ];then
+clear
+apt install git -y
+echo -e "\e[1;34m Downloading Latest Files..."
+git clone https://github.com/Hunter11821/HUNTBOMBER
+if [[ -s HUNTBOMBER/HuntBomber.sh ]];then
+cd HUNTBOMBER
+cp -r -f * .. > temp
+cd ..
+rm -rf  HUNTBOMBER >> temp
+rm hunter.11821 >> temp
+rm temp
+chmod +x HuntBomber.sh
+fi
+echo -e "\e[1;32m HUNTBOMBER Will Restart Now..."
+echo -e "\e[1;32m All The Required Packages Will Be Installed..."
+echo -e "\e[1;34m Press Enter To Proceed To Restart..."
+read a6
+./HuntBomber.sh
+exit
+elif [ $ch -eq 4 ];then
+clear
+echo -e "\e[1;33m"
+figlet HuntBomber
+echo -e "\e[1;34mCreated By \e[1;34m"
+toilet -f mono12 -F border HUNTER11821
+echo  " "
+echo -e "\e[1;32m                   Features\e[1;34m"
+echo -e "\e[1;37m  [+] Unlimited And Super-Fast Bombingb \e[0m"
+echo -e "\e[1;37m  [+] International Bombing \e[0m"
+echo -e "\e[1;37m  [+] Call Bombing \e[0m"
+echo -e "\e[1;37m  [+] Protection List \e[0m"
+echo -e "\e[1;37m  [+] Automated Future Updates \e[0m"
+echo -e "\e[1;37m  [+] Easy To Use And Embed in Code \e[0m"
+echo ""
+echo ""
+echo -e "\e[1;31m This Script is Only For Educational Purposes or To Prank.\e[0m"
+echo -e "\e[1;31m Do not Use This To Harm Others. \e[0m"
+echo -e "\e[1;31m I Am Not Responsible For The Misuse Of The Script. \e[0m"
+echo -e "\e[1;32m Make Sure To Update it If It Does not Work.\e[0m"
+echo  " "
+echo -e "\e[4;31m That's All !!!\e[0m"
+echo -e "\e[1;34m For Any Problem Contact Me on telegram \e[0m"
+echo -e "\e[1;41m    Telegram ID :- @Official_Hunter \e[0m"
+echo -e "\e[1;41m    Telegram Channle :- telegram.dog/c/Hunter11821 \e[0m"
+echo -e "\e[1;41m    YouTube Channle: https://www.youtube.com/c/UCndBK_Y6QHxLXZ2XLrf4zoQ \e[0m"
+echo " "echo "Press Enter To Go Home"
+read a3
+clear
+elif [ $ch -eq 5 ];then
+clear
+echo -e "\e[1;31m"
+figlet TBomb
+echo -e "\e[1;34m Created By \e[1;32m"
+toilet -f mono12 -F border HUNTER11821
+echo -e "\e[1;34m For Any Problem Contact Me on telegram \e[0m"
+echo -e "\e[1;41m    Telegram ID :- @Official_Hunter \e[0m"
+echo -e "\e[1;41m    Telegram Channle :- telegram.dog/c/Hunter11821 \e[0m"
+echo -e "\e[1;41m    YouTube Channle: https://www.youtube.com/c/UCndBK_Y6QHxLXZ2XLrf4zoQ \e[0m"
+echo " "
+exit 0
+else
+echo -e "\e[4;32m Invalid Input !!! \e[0m"
+echo "Press Enter To Go Home"
+read a3
+clear
+fi
+done
---
+++ README.md
@@ -0,0 +1,117 @@
+<h1 align="center">HuntBomber</h1>
+<p align="center">An open-source SMS/call bomber for Linux And Termux.</p><br>
+
+## Note:
+
+Due misusing of HuntBomber, several API's died. 
+Don't be afraid if you don't see all send messages.
+
+- The script requires working network connection to work.
+- No balance will be deducted for using this script to send SMS/calls.
+- While doing infinite bombing use 2-3 seconds delay and 10 to 20 threads for maximum performance.
+- Don't put spaces in between phone number (Ex- 99999 99999)
+- Make sure you are using the latest version of HuntBomber
+- Make sure you are using Python3.
+
+Here's how you can check it. Type this command in your terminal.
+```
+$ python -V
+```
+If output looks like `Python 3` - Congrats, Python 3 is installed properly.
+
+- Do not use this to harm others.
+- This script is only for educational purposes or to prank.
+- **None of the developers/contributors are responsible for the misuse of HuntBomber.**
+<br>
+
+## Features:
+
+- Lots of integrated SMS/call APIs
+- Unlimited (Limited against abusing) and super-fast bombing
+- International bombing available (APIS Dead. Try Your Luck.) 
+- Call bombing
+- Frequent updates
+- Automatic updating mechanism
+- Easy to use and embed in code
+
+## Usage:
+
+Run these commands to run HuntBomber
+
+### > For Termux:
+
+**Notice:** 
+
+git installation methods are not universal and do differ between distributions,
+so, installing git as per instructions below may not work.
+Please check out how to install `git` for your Linux distribution.
+Commands below provide instructions for Debian-based systems.
+
+To use the bomber type the following commands in Termux:
+```
+pkg install git
+pkg install python
+git clone https://github.com/HUNTER11821/HuntBomber.git
+cd HuntBomber
+chmod +x HuntBomber.sh
+./HuntBomber.sh
+```
+
+### > For Linux:
+
+**Notice:** 
+
+git installation methods are not universal and do differ between distributions,
+so, installing git as per instructions below may not work.
+Please check out how to install `git` for your Linux distribution.
+Commands below provide instructions for Debian-based systems.
+
+To use the bomber type the following commands in Linux terminal:
+```
+sudo apt install git
+git clone https://github.com/HUNTER11821/HuntBomber.git
+cd HUNTBOMBER
+chmod +x HuntBomber.sh
+sudo bash HuntBomber.sh
+```
+
+### > For macOS:
+
+To use the bomber type the following commands in macOS terminal:
+```
+# Install Brew: 
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install dependencys:
+
+brew install git
+brew install python3
+sudo easy_install pip
+sudo pip install --upgrade pip
+git clone https://github.com/HUNTER11821/HuntBomber.git
+cd HUNTBOMBER
+chmod +x HuntBomber.sh
+
+# Missing Tools on MacOS
+
+Toilet cannot be installed yet. But HuntBomber does still work.
+
+# Run HuntBomber:
+
+sudo bash HuntBomber.sh
+```
+
+#Join Telegram Channel
+
+<a href="https://t.me/hunter11821">JOIN MY TELEGRAM CHANNEL</a>
+
+
+# YOUTUBE CHANNEL
+
+<a href="https://www.youtube.com/channel/UCndBK_Y6QHxLXZ2XLrf4zoQ">Check Out My YouTube Channel</a>
+
+# HOW TO USE VIDEO
+
+<a href=" soon ">Check Out Video How to Use</a>
+
